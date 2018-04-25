723568e93bedbc9b4abac3454e4030b4a04b31bc
V <vickieverts1@gmail.com>
2018-04-24 21:52:23 -0600

    add image

--- DemintiaPhone/viewControllers/AddImageViewController.swift
+++ DemintiaPhone/viewControllers/AddImageViewController.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+import UIKit
+
+
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var chooseBuuton: UIButton!
+    var imagePicker = UIImagePickerController()
+    
+    @IBAction func btnClicked() {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+            print("Button capture")
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .savedPhotosAlbum;
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
+        self.dismiss(animated: true, completion: { () -> Void in
+            
+        })
+        
+        imageView.image = image
+    }
+}
