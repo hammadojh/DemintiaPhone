52d5db3a04898b3e5421d70968572c5082d61433
Omar <hammado92@gmail.com>
2018-04-29 19:50:12 -0600

    white status bar

--- DemintiaPhone/AppDelegate.swift
+++ DemintiaPhone/AppDelegate.swift
@@ -20,7 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // status bar
+        
+        UIApplication.shared.statusBarStyle = .lightContent
         
         // read contacts
         
